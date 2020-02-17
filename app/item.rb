a1231ddc90f3c9c363eb9e6d40cc849695488915
Matthew Jagiello <mjagiello@gmail.com>
2020-02-17 22:45:30 +0000

    Done.

--- app/item.rb
+++ app/item.rb
@@ -1,8 +1,23 @@
 class Item
   attr_accessor :name, :price
 
+  @@item = []
+
   def initialize(name,price)
     @name = name
     @price = price
   end
+
+  def call(env)
+  resp = Rack::Response.new
+  req = Rack::Request.new(env)
+
+  if req.path=="/songs"
+  else
+    resp.write "Route not found"
+    resp.status = 400
+  end
+
+      resp.finish
+    end
 end
