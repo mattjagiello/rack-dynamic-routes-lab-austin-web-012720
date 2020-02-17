f3b6fd333bab9df39608b44a83283e3dd5232d52
Matthew Jagiello <mjagiello@gmail.com>
2020-02-17 22:36:19 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -9,6 +9,7 @@ class Application
   if req.path.match("/items")
     item = req.params["item"]
     if @@items.include?(item)
+      resp.write item.price
     else
       resp.write "Item not found"
       resp.status = 400
