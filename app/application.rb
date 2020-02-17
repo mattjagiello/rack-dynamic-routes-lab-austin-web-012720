031b35e15bdd579bc399349ee708ed2f3c415a8f
Matthew Jagiello <mjagiello@gmail.com>
2020-02-17 22:33:34 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -7,6 +7,8 @@ class Application
   req = Rack::Request.new(env)
 
   if req.path.match("/items")
+    item = req.params["item"]
+    if @@items.include?()
   else
     resp.write "Route not found"
     resp.status = 404
