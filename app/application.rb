bc41bafa676fd1caaaa155161497612c32af1459
Matthew Jagiello <mjagiello@gmail.com>
2020-02-17 22:31:00 +0000

    Automatically backed up by Learn

---
+++ app/application.rb
@@ -0,0 +1,15 @@
+class Application
+
+  def call(env)
+  resp = Rack::Response.new
+  req = Rack::Request.new(env)
+
+  if req.path=="/songs"
+  else
+    resp.write "Route not found"
+    resp.status = 400
+  end
+
+      resp.finish
+    end
+  end
